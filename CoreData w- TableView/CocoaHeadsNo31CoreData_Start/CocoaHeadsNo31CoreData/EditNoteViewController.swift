0213e52536362308208aed8d57bb5e402c4fc389
Knight <horseman_culture@foxmail.com>
2017-08-30 18:44:33 +0800

    Update Start Project

--- CoreData w- TableView/CocoaHeadsNo31CoreData_Start/CocoaHeadsNo31CoreData/EditNoteViewController.swift
+++ CoreData w- TableView/CocoaHeadsNo31CoreData_Start/CocoaHeadsNo31CoreData/EditNoteViewController.swift
@@ -17,27 +17,15 @@ class EditNoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // MARK: - Fileprivate Method
 
     // MARK: - Action
     
     @IBAction func didTapUpdateToNow(_ sender: UIButton) {
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
